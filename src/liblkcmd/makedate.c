62532b83ea3cbbaa98a4c1887ec4c67af560ab19
David A.D. Mornao <david.a.morano@gmail.com>
2018-03-23 08:46:35 -0400

    sync w/ main base

--- src/liblkcmd/makedate.c
+++ src/liblkcmd/makedate.c
@@ -1,2 +1,2 @@
 const char liblkcmd_makedate[] =
-"@(#)liblkcmd                180314_2340:28_USEDT Rightcore Network Services" ;
+"@(#)liblkcmd                180323_0121:10_USEDT Rightcore Network Services" ;
